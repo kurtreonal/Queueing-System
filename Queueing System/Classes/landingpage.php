0ea58b77d9533288332e069f9639818a1a2436ac
Kurt Pascua <kurtreonal26@gmail.com>
2025-01-12 22:22:04 +0800

    Updated 120125

--- Queueing System/Classes/landingpage.php
+++ Queueing System/Classes/landingpage.php
@@ -12,11 +12,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <div class="img-container">
-        <div class="image"></div>
-        <div class="blur"></div>
-    </div>
-
     <!--Content-->
     <section class="container">
         <div class="table-container">
