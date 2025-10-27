cbdffa77e3661a617b4e74e4f4d08c3e99f2efcc
Kurt Pascua <kurtreonal26@gmail.com>
2025-10-27 12:38:41 +0800

    put it outside

---
+++ landingpage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CvSU Request Form</title>
+    <meta name="keywords" content="CvSU Form Request, CvSU File Request">
+    <meta name="description" content="CVSU Request Form It’s our business to know your business.">
+    <meta name="author" content="Landing Page designer (Pascua) - Group RedRivon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../Assets/cvsulogo.png" type="image/x-icon">
+    <link rel="stylesheet" href="./Styles/landingpage.css">
+</head>
+<body>
+<header class="nav">
+    <?php
+    include './Classes/nav.php';
+    ?>
+</header>
+    <div class="content">
+        <h1>Request Form</h1>
+        <p style="font-size: 20px;">IT'S OUR BUSINESS TO KNOW YOUR BUSINESS</p>
+        <div>
+            <a href="../Classes/request.php">
+                <button type="button">REQUEST NOW</button>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
